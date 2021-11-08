c339d20e9aade98d9bf26533614316f74712b046
ShirishaReddy09 <90176869+ShirishaReddy09@users.noreply.github.com>
2021-11-08 16:25:44 +0530

    Update Employee.java

--- EMS_springboot/src/main/java/EMS/EMS_springboot/model/Employee.java
+++ EMS_springboot/src/main/java/EMS/EMS_springboot/model/Employee.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
 
 @Entity
 @Table(name = "employees")
@@ -14,6 +16,7 @@ public class Employee {
 	private long id;
 	private String firstName;
 	private String lastName;
+  @Email
 	private String emailId;
 	private String dept;
 	private String designation;
