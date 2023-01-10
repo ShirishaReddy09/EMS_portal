16158b5c1e66d5c2be7b7d1316beef24355c5664
ShirishaReddy09 <pathishirisha9@gmail.com>
2023-01-10 19:11:20 +0530

    main_method_gui

--- EMS_springboot/src/main/java/EMS/EMS_springboot/EmsSpringbootApplication.java
+++ EMS_springboot/src/main/java/EMS/EMS_springboot/EmsSpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmsSpringbootApplication {
 //main method
+//main_method_git_gui
 	public static void main(String[] args) {
 		SpringApplication.run(EmsSpringbootApplication.class, args);
 	}
