a26c27ddb7b404e1668998641702a4314c39e99a
Pathi <PATHI.SHIRISHA@chubb.com>
2023-01-10 19:00:29 +0530

    main_method

--- EMS_springboot/src/main/java/EMS/EMS_springboot/EmsSpringbootApplication.java
+++ EMS_springboot/src/main/java/EMS/EMS_springboot/EmsSpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmsSpringbootApplication {
-
+//main method
 	public static void main(String[] args) {
 		SpringApplication.run(EmsSpringbootApplication.class, args);
 	}
